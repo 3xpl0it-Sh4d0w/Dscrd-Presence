7c1702da7869ecb922179cc42dd2ccca80ee0756
3xpl0it.Sh4d0w <3xpl0it.sh4d0w@pm.me>
2022-04-27 16:18:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
-// NodeJS "net" Module :
-const NetSrv = require('net');
+// Importe le module "WebSocket" :
+import { WebSocketServer } from "ws";
+// Créer un Socket sur le port 3000 :
+const Server = new WebSocketServer({ port: 3000 });
